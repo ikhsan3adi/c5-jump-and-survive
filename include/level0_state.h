a80e16e12f3345a1cf0524e66828a6c7a868a5f8
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-03-03 04:02:36 +0700

    Initialize level 0

---
+++ include/level0_state.h
@@ -0,0 +1,14 @@
+#ifndef LEVEL0_STATE_H
+#define LEVEL0_STATE_H
+
+#include "game_state.h"
+
+extern GameState level0_state;
+
+void level0_init();
+void level0_handle_input(SDL_Event *);
+void level0_update();
+void level0_render(SDL_Renderer *);
+void level0_cleanup();
+
+#endif
