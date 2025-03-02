c7468f68c9d28fb117f10e9a4d73f447ae3a364a
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-03-02 15:37:05 +0700

    refactor(game_state): update game state management

--- src/game_state.c
+++ src/game_state.c
@@ -2,7 +2,7 @@
 
 GameState *current_state;
 
-void change_state(GameState *new_state)
+void change_game_state(GameState *new_state)
 {
   if (current_state)
   {
@@ -10,4 +10,15 @@ void change_state(GameState *new_state)
   }
   current_state = new_state;
   current_state->init(); // initialize new state
+}
+
+GameState *get_current_game_state()
+{
+  if (!current_state)
+  {
+    SDL_Log("`current_state` has not been initialized!, call `change_game_state` first.");
+    exit(1);
+  }
+
+  return current_state;
 }
