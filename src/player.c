36da682981e33f4ba071a97f3c995e052db35bf5
muhamadSyahid <muhamadsyahid2016@gmail.com>
2025-03-03 10:09:40 +0700

    fix game physics (collision)

--- src/player.c
+++ src/player.c
@@ -11,9 +11,13 @@ void handle_player_input(Entity *player, SDL_Event *event)
     if (event->key.key == SDLK_RIGHT)
       player->physics.velocity_x = player->physics.speed;
     if (event->key.key == SDLK_SPACE && player->physics.velocity_y == 0)
-      player->physics.velocity_y = -3000; // Jump
+      player->physics.velocity_y = -5000; // Jump
+  } 
+  else {
+    player->physics.velocity_x = 0;
   }
 }
+
 
 void destroy_player(Entity *player)
 {
