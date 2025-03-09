30f7ba389f2a4c40a38763e5e4796f6fdbd929aa
muhamadSyahid <muhamadsyahid2016@gmail.com>
2025-03-09 11:31:23 +0700

    Add double input handler

--- include/player.h
+++ include/player.h
@@ -3,6 +3,9 @@
 
 #include "entity.h"
 
+#define PLAYER_JUMP -5000.0f
+#define PLAYER_MOVE_MULTIPLIER 2000.0f
+
 void handle_player_input(Entity *player, SDL_Event *event);
 
 void destroy_player(Entity *player);
--- src/player.c
+++ src/player.c
@@ -2,19 +2,36 @@
 #include <SDL3/SDL.h>
 #include "player.h"
 
+// array untuk menyimpan status tombol
+bool key_state[SDL_SCANCODE_COUNT];
+
 void handle_player_input(Entity *player, SDL_Event *event)
 {
   if (event->type == SDL_EVENT_KEY_DOWN)
   {
-    if (event->key.key == SDLK_LEFT)
-      player->physics.velocity_x = -player->physics.speed;
-    if (event->key.key == SDLK_RIGHT)
-      player->physics.velocity_x = player->physics.speed;
-    if (event->key.key == SDLK_SPACE && player->physics.velocity_y == 0)
-      player->physics.velocity_y = -5000; // Jump
-  } 
-  else if (event->type == SDL_EVENT_KEY_UP) {
-    if (event->key.key == SDLK_RIGHT || event->key.key == SDLK_LEFT) {
+    key_state[event->key.scancode] = true; // tombol ditekan
+
+    if (key_state[SDL_SCANCODE_LEFT])
+    {
+      player->physics.velocity_x = -player->physics.speed * PLAYER_MOVE_MULTIPLIER;
+    }
+    else if (key_state[SDL_SCANCODE_RIGHT])
+    {
+      player->physics.velocity_x = player->physics.speed * PLAYER_MOVE_MULTIPLIER;
+    }
+    if (key_state[SDL_SCANCODE_SPACE] && player->physics.velocity_y == 0)
+    {
+      player->physics.velocity_y = PLAYER_JUMP;
+    }
+  }
+  else if (event->type == SDL_EVENT_KEY_UP)
+  {
+    key_state[event->key.scancode] = false; // tombol dilepas
+
+    // Cek apakah masih ada tombol lain (arrow kiri dan kanan) yang ditekan
+    // sebelum menghentikan player
+    if (!key_state[SDL_SCANCODE_LEFT] && !key_state[SDL_SCANCODE_RIGHT])
+    {
       player->physics.velocity_x = 0;
     }
   }
