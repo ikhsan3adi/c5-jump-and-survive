0eb0958be3f29a67ef7ac3b824ccc61ed25b311b
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-05-28 11:33:12 +0700

    refactor: change main function to standard main
    Updates Game struct to include SDL_Event and is_exit.
    Changes SDL_AppInit, SDL_AppEvent, SDL_AppIterate, SDL_AppQuit
    to standard main function.

--- include/game.h
+++ include/game.h
@@ -11,6 +11,8 @@ typedef struct
 {
   SDL_Window *window;
   SDL_Renderer *renderer;
+  SDL_Event *event;
+  bool is_exit;
   bool is_physics_paused; // untuk memberhentikan update physics (set delta_time ke 0)
 } Game;
 
--- src/main.c
+++ src/main.c
@@ -1,6 +1,4 @@
-#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 
 #include "game.h"
 #include "game_state.h"
@@ -12,77 +10,83 @@
 
 Uint64 last_time;
 
-/* This function runs once at startup. */
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+int main()
 {
+  Game *game = NULL;
+  GameState *current_state = NULL;
+
+  Uint64 current_time = 0;
+  double delta_time; // delta_time dalam satuan detik
+
   initialize("Jump & Survive", "1.0", "com.c5.jump-and-survive");
 
   create_game_instance("Jump & Survive", SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  game = get_game_instance();
 
   // load assets (fonts, sfx)
   init_font();
   init_audio();
 
-  last_time = SDL_GetPerformanceCounter();
-
   load_levels("assets/levels/");
 
   change_game_state(&menu_state);
 
-  return SDL_APP_CONTINUE;
-}
+  last_time = SDL_GetPerformanceCounter();
 
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
-  if (event->type == SDL_EVENT_QUIT)
+  while (!game->is_exit)
   {
-    return SDL_APP_SUCCESS;
+    current_state = get_current_game_state();
+
+    /** =========================== INPUT/EVENT HANDLING ============================= */
+
+    if (SDL_PollEvent(game->event))
+    {
+      if (game->event->type == SDL_EVENT_QUIT)
+      {
+        game->is_exit = true;
+        break;
+      }
+
+      current_state->handle_input(game->event);
+    }
+
+    /** ========================= GAME LOOP & UPDATE PHYSICS ========================= */
+
+    // mendapatkan time/counter saat ini
+    current_time = SDL_GetPerformanceCounter();
+
+    if (game->is_physics_paused)
+    {
+      delta_time = 0.0f;
+      resume_physics();
+    }
+    else
+    {
+      // membuat delta time (waktu antara frame saat ini denga frame sebelumnya)
+      delta_time = (double)((current_time - last_time) / (double)SDL_GetPerformanceFrequency());
+    }
+
+    last_time = current_time;
+
+    current_state->update(delta_time);
+
+    /** =============================== RENDERING =================================== */
+
+    current_state->render(game->renderer);
+    SDL_RenderPresent(game->renderer);
+
+    SDL_Delay(16); // Delay untuk mencapai ~60 FPS
   }
 
-  GameState *current_state = get_current_game_state();
+  /** ========== QUIT ========== */
+  SDL_Log("Exiting game...");
 
-  current_state->handle_input(event);
-
-  return SDL_APP_CONTINUE;
-}
-
-/* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-  Game *game = get_game_instance();
-
-  // membuat delta time (waktu antara frame saat ini denga frame sebelumnya)
-  Uint64 current_time = SDL_GetPerformanceCounter();
-  double delta_time; // dalam satuan detik
-
-  if (game->is_physics_paused)
-  {
-    delta_time = 0.0f;
-    resume_physics();
-  }
-  else
-  {
-    delta_time = (double)((current_time - last_time) / (double)SDL_GetPerformanceFrequency());
-  }
-
-  last_time = current_time;
-
-  SDL_Renderer *renderer = game->renderer;
-
-  GameState *current_state = get_current_game_state();
-
-  current_state->update(delta_time);
-  current_state->render(renderer);
-  SDL_RenderPresent(renderer);
-
-  return SDL_APP_CONTINUE;
-}
-
-/* This function runs once at shutdown. */
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
+  // membersihkan resource
+  current_state->cleanup();
   clean_up_ui();
   cleanup_audio();
-  /* SDL will clean up the window/renderer for us. */
+  SDL_Quit();
+
+  return 0;
 }
